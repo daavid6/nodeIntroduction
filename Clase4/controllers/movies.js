f49154133600c250cc0119bf819f15c3a0de5684
David Ruiz dC <drd733@alumnos.unican.es>
2024-06-17 16:38:52 +0200

    Clase 4 Finalizada

---
+++ Clase4/controllers/movies.js
@@ -0,0 +1,50 @@
+import { MovieModel } from "../models/movie.js";
+import { validateMovie, validatePartialMovie } from '../schemas/movies.js';
+
+export class MovieController {
+  static async getAll (req, res) {
+    const { genre } = req.query;
+    const movies = await MovieModel.getAll({ genre })
+    res.json(movies)
+  }
+
+  static async getById (req, res) {
+    const { id } = req.params
+    const movie = await MovieModel.getById({ id })
+    if (movie) return res.json(movie)
+    res.status(404).json({ message: 'Movie not found' })
+  }
+
+  static async create (req, res) {
+    const resultado = validateMovie(req.body)
+  
+    if (resultado.error) {
+      return res.status(400).json({ error: JSON.parse(resultado.error.message) })
+    }
+  
+    const newMovie = await MovieModel.create({input: resultado.data })
+    res.status(201).json(newMovie)
+  }
+
+  static async delete (req, res) {
+    const { id } = req.params
+    const resultado = await MovieModel.delete({id})
+  
+    if (resultado) return res.json({ message: 'Movie deleted' }) 
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  static async update (req, res) {
+    const { id } = req.params;
+    const resultado = validatePartialMovie(req.body);
+  
+    if (!resultado.success) {
+      return res.status(400).json({ error: JSON.parse(resultado.error.message) })
+    }
+  
+    const updateMovie = await MovieModel.update( { id, input: resultado.data })
+  
+    if (updateMovie === false) return res.status(404).json({ message: 'Movie not found' })
+    res.json(updateMovie)
+  }
+}
--- Clase4/routes/movies.js
+++ Clase4/routes/movies.js
@@ -1,97 +1,14 @@
 import { Router } from 'express'
-import { randomUUID } from 'node:crypto';
-import { validateMovie, validatePartialMovie } from './schemas/movies.js';
-
-//  Aún experimental pero ya funciona, para importar json:
-import movies from './movies.json' with { type: 'json'};
-/* 
-  --- Alternativa 1 ---
-  import fs from 'node:fs'
-  const movies = JSON.parse(fs.readFileSync('./movies.json'))
-
-
-  --- Alternativa 2 ---
-  import { createRequire } from 'node:module'
-  const require = createRequire(import.meta.url)
-  const movies = require('./movies.json'
-*/
-
+import { MovieController } from '../controllers/movies.js';
 
 export const moviesRouter = Router()
 
-moviesRouter.get('/', (req, res) => {
-  const { genre } = req.query;
-  if (genre) {
-    const filteredMovies = movies.filter((movie) =>
-      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
-    );
-    return res.json(filteredMovies);
-  }
+moviesRouter.get('/', MovieController.getAll)
 
-  res.json(movies);
-})
+moviesRouter.get('/:id', MovieController.getById)
 
-moviesRouter.get('/:id', (req, res) => {
-  const { id } = req.params
-  const movie = movies.find((movie) => movie.id === id);
-  if (movie) return res.json(movie)
+moviesRouter.post('/', MovieController.create)
 
-  res.status(404).json({ message: 'Movie not found' })
-})
+moviesRouter.delete('/:id', MovieController.delete)
 
-// Añadir película
-moviesRouter.post('/', (req, res) => {
-  const resultado = validateMovie(req.body)
-
-  if (resultado.error) {
-    return res.status(400).json({ error: JSON.parse(resultado.error.message) })
-  }
-
-  const newMovie = {
-    id: randomUUID(),
-    ...resultado.data,
-  }; 
-
-  movies.push(newMovie);
-  res.status(201).json(newMovie)
-})
-
-
-// Editar película
-moviesRouter.patch('/:id', (req, res) => {
-  const { id } = req.params;
-  const resultado = validatePartialMovie(req.body);
-
-  if (!resultado.success) {
-    return res.status(400).json({ error: JSON.parse(resultado.error.message) })
-  }
-
-
-  const movieIndex = movies.findIndex((movie) => movie.id === id);
-
-  if (movieIndex === -1) {
-    return res.status(404).json({ message: 'Movie not found' })
-  }
-
-  const updateMovie = {
-    ...movies[movieIndex],
-    ...resultado.data
-  }
-
-  movies[movieIndex] = updateMovie
-
-  res.json(movies[movieIndex])
-})
-
-moviesRouter.delete('/:id', (req, res) => {
-  const { id } = req.params
-  const movieIndex = movies.findIndex((movie) => movie.id === id);
-
-  if (movieIndex === -1) {
-    return res.status(404).json({ message: 'Movie not found' })
-  }
-
-  movies.splice(movieIndex, 1);
-
-  return res.json({ message: 'Movie deleted' })
-})
+moviesRouter.patch('/:id', MovieController.update)
